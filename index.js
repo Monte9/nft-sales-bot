15d7d1032b7aeb85853688cab38eea89ba678338
Monte Thakkar <manthan.thakkar@gmail.com>
2021-07-13 12:40:31 +0530

    added getNFT() for OpenSea Sale events

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+const OpenSeaAPI = require('./src/OpenSeaAPI');
 const TwitterAPI = require('./src/TwitterAPI');
 
 class TwitterMcBot {
   constructor() {
     this.asset_range = 10
-  }
 
-  runInstance() {
-    console.log("Running Twitter McBot")
-
-    const twitterAPI = new TwitterAPI(
+    this.twitterAPI = new TwitterAPI(
       'H8uM5UikvJpbfBVyqwoNfK5au',
       'SJvPQ1lN18Cs1P58qryskzkp5DsnRO5tbyNy0i3Yuve7qoN6HU',
       '928651527383744512-m7XZP6R1sdEHRhIoT9gBRDwfoBflAAC',
       'ZuU67EVrrSuZDQiIM8RFl4QHanKr9ZMDAstCQOF5cYNT0'
     )
 
-    twitterAPI.postTweet("I am building a Twitter bot that will tweets all NFT sales along with the profit/loss flip % for CryptoPunks, Bored Apes & Cool Cats.")
+    this.openSeaAPI = new OpenSeaAPI();
+  }
+
+  async runInstance() {
+    console.log("Running Twitter McBot")
+
+    // Get latest events for the NFT
+    let saleEvents = await this.openSeaAPI.getNFT()
+
+    console.log(saleEvents)
+
+    // Post a tweet
+    // this.twitterAPI.postTweet("I am building a Twitter bot that will tweets all NFT sales along with the profit/loss flip % for CryptoPunks, Bored Apes & Cool Cats.")
   }
 }
 
---
+++ src/OpenSeaAPI.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+
+module.exports = class OpenSeaAPI {
+  constructor() {
+    this.boredApeContract = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
+    this.blueBeamsApeId = '5828'
+    this.eventType = 'successful'
+  }
+
+  async getNFT() {
+    const url = `https://api.opensea.io/api/v1/events?only_opensea=false&offset=0&limit=20&asset_contract_address=${this.boredApeContract}&token_id=${this.blueBeamsApeId}&event_type=${this.eventType}`;
+    const options = {
+      method: 'GET', 
+      headers: {
+        Accept: 'application/json'
+      }
+    };
+    
+    return await fetch(url, options)
+      .then(res => res.json())
+      .catch(err => console.error('error:' + err));
+  }
+}
