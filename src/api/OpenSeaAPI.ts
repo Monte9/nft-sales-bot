031d5ab1d359188cca5a5a218f8fa0e24a1344f6
Monte Thakkar <manthan.thakkar@gmail.com>
2021-07-16 14:24:43 +0530

    created OPENSEA_CONTRACTS constant

--- src/api/OpenSeaAPI.ts
+++ src/api/OpenSeaAPI.ts
@@ -1,24 +1,14 @@
 import fetch from 'node-fetch';
 
-const COLLECTIONS = {
-  'boredapeyachtclub': {
-    contract: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
-  },
-  'cool-cats-nft': {
-    contract: '0x1a92f7381b9f03921564a437210bb9396471050c',
-  },
-  'cryptopunks': {
-    contract: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
-  },
-  'meebits': {
-    constract: '0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7'
-  }
-}
-
 export default class OpenSeaAPI {
-  smartContract = '0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7'
-  tokenId = '8426'
+  smartContract = null
+  tokenId = null
   eventType = 'successful'
+
+  constructor(smartContract, tokenId) {
+    this.smartContract = smartContract;
+    this.tokenId = tokenId;
+  }
   
   async getNFT() {
     const url = 'https://api.opensea.io/api/v1/events';
---
+++ src/shared/constants.ts
@@ -0,0 +1,10 @@
+export const OPENSEA_CONTRACTS = [
+  // boredapeyachtclub
+  '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
+  // cool-cats-nft
+  '0x1a92f7381b9f03921564a437210bb9396471050c',
+  // cryptopunks
+  '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
+  // meebits
+  '0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7',
+]
