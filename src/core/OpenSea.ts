1c9c4751bf22876946d128f426ca43f30e656260
Monte Thakkar <manthan.thakkar@gmail.com>
2021-08-20 08:43:39 +0530

    handle empty asset for BAYC - works now

--- src/core/OpenSea.ts
+++ src/core/OpenSea.ts
@@ -1,62 +1,67 @@
 import { Collection, Asset, User, PaymentToken, Sale, Transaction } from "../types/OpenSeaSale";
 
 export function parseSales(saleEvents): Sale[] {
-  let sales: Sale[] = saleEvents.map(saleEvent => {
-    let collection: Collection = {
-      address: saleEvent.asset.asset_contract.address,
-      symbol: saleEvent.asset.asset_contract.symbol,
-      name: saleEvent.asset.collection.name,
-      twitterUsername: saleEvent.asset.collection.twitter_username,
-      slug: saleEvent.asset.collection.slug,
+  return saleEvents.reduce((acc, saleEvent) => {
+    // Sometimes asset can be empty for some BAYC tokens
+    if (saleEvent.asset && saleEvent.transaction) {
+      let collection: Collection = {
+        address: saleEvent.asset.asset_contract.address,
+        symbol: saleEvent.asset.asset_contract.symbol,
+        name: saleEvent.asset.collection.name,
+        twitterUsername: saleEvent.asset.collection.twitter_username,
+        slug: saleEvent.asset.collection.slug,
+      }
+
+      let asset: Asset = {
+        tokenId: saleEvent.asset.token_id,
+        image: saleEvent.asset.image_url,
+        link: saleEvent.asset.permalink,
+        collection,
+      }
+
+      let buyer: User = {
+        address: saleEvent.winner_account.address,
+        username: saleEvent.winner_account.user && saleEvent.winner_account.user.username || null
+      }
+
+      let seller: User = {
+        address: saleEvent.seller && saleEvent.seller.address || "Unknown",
+        username: saleEvent.seller && saleEvent.seller.user && saleEvent.seller.user.username || null
+      }
+
+      let paymentToken: PaymentToken = null
+      if (saleEvent.payment_token !== null) {
+        paymentToken = {
+          symbol: saleEvent.payment_token.symbol,
+          name: saleEvent.payment_token.name,
+          imageUrl: saleEvent.payment_token.image_url,
+          decimals: saleEvent.payment_token.decimals,
+          usdPrice: Math.round(Number(saleEvent.payment_token.usd_price) * 100) / 100
+        } 
+      }
+
+      let transaction: Transaction = {
+        id: saleEvent.transaction.id,
+        block_hash: saleEvent.transaction.block_hash,
+        block_number: saleEvent.transaction.block_number,
+        timestamp: saleEvent.transaction.timestamp,
+        transaction_hash: saleEvent.transaction.transaction_hash,
+        transaction_index: saleEvent.transaction.transaction_index,
+      }
+
+      const sale = {
+        asset,
+        buyer,
+        seller,
+        paymentToken,
+        salePrice: saleEvent.total_price / Math.pow(10, paymentToken && paymentToken.decimals || 18),
+        saleId: saleEvent.id,
+        transaction
+      }
+
+      acc.push(sale)
     }
 
-    let asset: Asset = {
-      tokenId: saleEvent.asset.token_id,
-      image: saleEvent.asset.image_url,
-      link: saleEvent.asset.permalink,
-      collection,
-    }
-
-    let buyer: User = {
-      address: saleEvent.winner_account.address,
-      username: saleEvent.winner_account.user && saleEvent.winner_account.user.username || null
-    }
-
-    let seller: User = {
-      address: saleEvent.seller && saleEvent.seller.address || "Unknown",
-      username: saleEvent.seller && saleEvent.seller.user && saleEvent.seller.user.username || null
-    }
-
-    let paymentToken: PaymentToken = null
-    if (saleEvent.payment_token !== null) {
-      paymentToken = {
-        symbol: saleEvent.payment_token.symbol,
-        name: saleEvent.payment_token.name,
-        imageUrl: saleEvent.payment_token.image_url,
-        decimals: saleEvent.payment_token.decimals,
-        usdPrice: Math.round(Number(saleEvent.payment_token.usd_price) * 100) / 100
-      } 
-    }
-
-    let transaction: Transaction = {
-      id: saleEvent.transaction.id,
-      block_hash: saleEvent.transaction.block_hash,
-      block_number: saleEvent.transaction.block_number,
-      timestamp: saleEvent.transaction.timestamp,
-      transaction_hash: saleEvent.transaction.transaction_hash,
-      transaction_index: saleEvent.transaction.transaction_index,
-    }
-
-    return {
-      asset,
-      buyer,
-      seller,
-      paymentToken,
-      salePrice: saleEvent.total_price / Math.pow(10, paymentToken && paymentToken.decimals || 18),
-      saleId: saleEvent.id,
-      transaction
-    }
-  })
-
-  return sales
+    return acc;
+  }, [])
 }
